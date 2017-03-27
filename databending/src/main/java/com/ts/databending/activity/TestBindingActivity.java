bbef3d28de08162fbf6962297047669b799a7938
shaowei <just_tanks@163.com>
2017-03-27 16:10:45 +0800

    delete the wrong code about the project

--- databending/src/main/java/com/ts/databending/activity/TestBindingActivity.java
+++ databending/src/main/java/com/ts/databending/activity/TestBindingActivity.java
@@ -1,32 +1,13 @@
 package com.ts.databending.activity;
 
 import android.databinding.DataBindingUtil;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTabHost;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ListView;
 
-import com.google.gson.Gson;
 //import com.ts.databending.BR;
 import com.ts.databending.R;
-import com.ts.databending.bean.FormListnew;
-import com.ts.databending.bean.User;
-import com.ts.databending.data.BaseData;
-
-
-import com.ts.databending.utils.Xutils;
-
-import org.xutils.common.Callback;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class TestBindingActivity extends AppCompatActivity {
     FragmentManager fragmentManager;
