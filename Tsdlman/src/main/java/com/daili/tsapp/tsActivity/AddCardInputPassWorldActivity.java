4daffb2bbb44416a2290390c983842ff87ca80fe
shaowei <just_tanks@163.com>
2017-03-28 08:56:30 +0800

    add the pass

--- Tsdlman/src/main/java/com/daili/tsapp/tsActivity/AddCardInputPassWorldActivity.java
+++ Tsdlman/src/main/java/com/daili/tsapp/tsActivity/AddCardInputPassWorldActivity.java
@@ -15,6 +15,7 @@ import com.daili.tsapp.tsBase.BaseData;
 import com.daili.tsapp.utils.NetUtils;
 import com.daili.tsapp.utils.SystemUtil;
 import com.google.gson.Gson;
+
 import org.xutils.common.Callback;
 
 import java.util.HashMap;
@@ -26,62 +27,65 @@ import java.util.Map;
 public class AddCardInputPassWorldActivity extends BaseActivity {
 
     PwdEditText editText;
-    String cardnum="";
+    String cardnum = "";
     String cardcity;
-    SystemUtil su=new SystemUtil(this);
-    ProgressDialog waitDialog=null;
+    SystemUtil su = new SystemUtil(this);
+    ProgressDialog waitDialog = null;
     RelativeLayout back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card_input_pass_world);
         editText = (PwdEditText) this.findViewById(R.id.input_passpet_pwd);
-        back= (RelativeLayout) this.findViewById(R.id.inputpass_back);
+        back = (RelativeLayout) this.findViewById(R.id.inputpass_back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
             }
         });
-        cardnum=getIntent().getStringExtra("cardnum");
-        cardcity=getIntent().getStringExtra("cardcity");
+        cardnum = getIntent().getStringExtra("cardnum");
+        cardcity = getIntent().getStringExtra("cardcity");
         editText.setOnInputFinishListener(new PwdEditText.OnInputFinishListener() {
             @Override
             public void onInputFinish(String password) {
-                 addCardToNet(password);
+                addCardToNet(password);
             }
         });
     }
-  ///waiter_id/71/brank_name/1%E5%93%88%E5%93%88%E5%93%88/card_num/11111111111111111111
-    //还缺少一个密码
+
+    ///waiter_id/71/brank_name/1%E5%93%88%E5%93%88%E5%93%88/card_num/11111111111111111111
+    //payment_password  访问网络 添加银行卡
+    Callback.Cancelable callback;
     private void addCardToNet(String pass) {
-        if(cardcity!=null&&cardnum!=null&&pass!=null){
-            waitDialog=ProgressDialog.show(this,"","正在添加银行卡");
+        if (cardcity != null && cardnum != null && pass != null) {
+            waitDialog = ProgressDialog.show(this, "", "正在添加银行卡");
             waitDialog.show();
-            Map<String,Object> parms=new HashMap<>();
-             parms.put("waiter_id",su.showUid());
-            parms.put("brank_name",cardcity);
-            parms.put("card_num",cardnum);
-            parms.put("withdrawals_password",pass);
-            NetUtils.Post(BaseData.ADDCARD, parms, new Callback.CommonCallback<String>() {
+            Map<String, Object> parms = new HashMap<>();
+            parms.put("waiter_id", su.showUid());
+            parms.put("brank_name", cardcity);
+            parms.put("card_num", cardnum);
+            parms.put("payment_password", pass);
+            callback = NetUtils.Post(BaseData.ADDCARD, parms, new Callback.CommonCallback<String>() {
                 @Override
                 public void onSuccess(String result) {
-                    Gson gson=new Gson();
-                    if(result.substring(0,18).contains("Success")){
-                        AddCardBean bean=gson.fromJson(result,AddCardBean.class);
-                        if("Success".equals(bean.getFlag())){
-                            Intent intent=new Intent(AddCardInputPassWorldActivity.this,AddcardDoneActivity.class);
+                    Gson gson = new Gson();
+                    if (result.substring(0, 18).contains("Success")) {
+                        AddCardBean bean = gson.fromJson(result, AddCardBean.class);
+                        if ("Success".equals(bean.getFlag())) {
+                            Intent intent = new Intent(AddCardInputPassWorldActivity.this, AddcardDoneActivity.class);
                             startActivity(intent);
                         }
-                    }else {
-                        NetError error=gson.fromJson(result,NetError.class);
+                    } else {
+                        NetError error = gson.fromJson(result, NetError.class);
                         toast(error.getMsg());
                     }
                 }
 
                 @Override
                 public void onError(Throwable ex, boolean isOnCallback) {
-
+                    toast(getResources().getString(R.string.net_err));
                 }
 
                 @Override
@@ -92,10 +96,19 @@ public class AddCardInputPassWorldActivity extends BaseActivity {
                 @Override
                 public void onFinished() {
                     waitDialog.dismiss();
-
                 }
             });
 
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (waitDialog.isShowing()) {
+            waitDialog.dismiss();
+            callback.cancel();
+        } else {
+            super.onBackPressed();
         }
 
     }
