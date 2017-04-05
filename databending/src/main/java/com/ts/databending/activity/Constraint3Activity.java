c400c77c400bb18beacc6b3e7d06d539d4bc7726
shaowei <just_tanks@163.com>
2017-04-05 16:41:18 +0800

    修改了状态栏 修改圆形头像的实现方式

---
+++ databending/src/main/java/com/ts/databending/activity/Constraint3Activity.java
@@ -0,0 +1,33 @@
+package com.ts.databending.activity;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.ts.databending.R;
+import com.ts.databending.databinding.ActivityConstraint3Binding;
+//databinding 和 constraintlayout 同时进行使用的效果非常好
+public class Constraint3Activity extends AppCompatActivity {
+     ActivityConstraint3Binding b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_constraint3);
+        b= DataBindingUtil.setContentView(this,R.layout.activity_constraint3);
+        b.bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Constraint3Activity.this, "hhhh", Toast.LENGTH_SHORT).show();
+            }
+        });
+        b.bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Constraint3Activity.this, "这下好玩了", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
