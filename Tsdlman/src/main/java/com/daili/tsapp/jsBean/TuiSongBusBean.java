23bb963081363617a8e8228cb7d81cf074feba59
shaowei <just_tanks@163.com>
2017-07-10 08:57:11 +0800

    处理应用内推送的展示

---
+++ Tsdlman/src/main/java/com/daili/tsapp/jsBean/TuiSongBusBean.java
@@ -0,0 +1,21 @@
+package com.daili.tsapp.jsBean;
+
+/**
+ * Created by Administrator on 2017/7/8.
+ */
+
+public class TuiSongBusBean {
+    int ss;
+
+    public TuiSongBusBean(int ss) {
+        this.ss = ss;
+    }
+
+    public int getSs() {
+        return ss;
+    }
+
+    public void setSs(int ss) {
+        this.ss = ss;
+    }
+}
